b551acfaa466dd76432d8adb8f5079a2578cba34
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-06-24 19:51:40 +0200

    init

---
+++ src/simple/date/CurrentDate.java
@@ -0,0 +1,29 @@
+package simple.date;
+
+public class CurrentDate {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
