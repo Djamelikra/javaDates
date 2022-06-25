cb641fe82e29c491ee5a9c0d26d1f75a506681a8
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-06-25 19:43:28 +0200

    date + h + doc

--- src/simple/date/CurrentDate.java
+++ src/simple/date/CurrentDate.java
@@ -2,7 +2,7 @@ package simple.date;
 
 import java.awt.Toolkit;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -26,17 +26,19 @@ public class CurrentDate {
 	}
 
 	public void dateFormatter() {
-		DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
-		 
-		String formattedDate = formatter.format(LocalDate.now());
-		System.out.println(formattedDate);
+		DateTimeFormatter dtfDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(dtfDate.format(LocalDateTime.now()));
+		DateTimeFormatter dtfHour = DateTimeFormatter.ofPattern("HH:mm:ss");
+		System.out.println(dtfHour.format(LocalDateTime.now()));
 	}
+	
 
 
 	public static void main(String[] args) {
 		CurrentDate cd = new CurrentDate();
-		cd.tryDate();
-		cd.currDate();
+//		cd.tryDate();
+//		cd.currDate();
+		cd.dateFormatter();
 		
 
 	}
