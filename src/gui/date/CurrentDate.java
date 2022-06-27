9cf3675435abb5a3d40b0e32968cc38e456f9265
Djamelikra <91476697+Djamelikra@users.noreply.github.com>
2022-06-27 19:08:48 +0200

    cr.displayV3();

--- src/gui/date/CurrentDate.java
+++ src/gui/date/CurrentDate.java
@@ -1,12 +1,11 @@
 package gui.date;
 
+import java.awt.Font;
 import java.awt.Toolkit;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
-
-
 
 /**
  * @author Djamel
@@ -56,8 +55,34 @@ public class CurrentDate {
 		Toolkit.getDefaultToolkit().beep();
 		JOptionPane.showMessageDialog(null, display, "Numbers List V2:", -1); // PLAIN_MESSAGE
 
-		System.exit(0);
+//		System.exit(0);
 
+	}
+
+	/**
+	 * customize a JTextArea
+	 */
+	public void displayV3() {
+		JTextArea display = new JTextArea(10, 30);
+		String texte = "nombre\tdouble\tquart\n";
+
+		for (int i = 1; i < 7; i++) {
+			int val1 = i * 2;
+			double val2 = i / 4.0;
+			texte += "\n" + i + "\t" + val1 + "\t" + val2;
+		}
+
+		display.setFont(new Font("Consolas", Font.ITALIC, 14));
+		display.setText(texte);
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null, display, "Numbers List Consolas:", -1); // PLAIN_MESSAGE
+
+		display.setFont(new Font("Algerian", Font.PLAIN, 24));
+		display.setText(texte);
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null, display, "Numbers List Algerian:", -1); // PLAIN_MESSAGE
+
+		System.exit(0);
 	}
 
 	public static void main(String[] args) {
@@ -70,6 +95,7 @@ public class CurrentDate {
 		CurrentDate cr = new CurrentDate();
 		cr.display();
 		cr.displayV2();
+		cr.displayV3();
 	}
 
 }
